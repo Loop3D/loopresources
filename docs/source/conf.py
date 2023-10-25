ac717ab7e4564d085d39be58062e3eae9e3129f5
Lachlan Grose <lachlan.grose@monash.edu>
2023-10-26 10:33:17 +1100

    docs: adding sphinx setup from LS

---
+++ docs/source/conf.py
@@ -0,0 +1,85 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+import loopresources as lr
+
+project = "Resources"
+copyright = "2023, Lachlan Grose"
+author = "Lachlan Grose"
+release = lr.__version__
+
+# -- General configuration ---------------------------------------------------
+autoclass_content = "both"  # include both class docstring and __init__
+autodoc_default_flags = [
+    # Make sure that any autodoc declarations show the right members
+    "members",
+    "inherited-members",
+    "private-members",
+    "show-inheritance",
+]
+autosummary_generate = True  # Make _autosummary files and include them
+napoleon_numpy_docstring = True  # False  # Force consistency, leave only Google
+napoleon_use_rtype = False  # More legible
+autosummary_imported_members = True
+autosummary_ignore_module_all = False
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    # Need the autodoc and autosummary packages to generate our docs.
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    # The Napoleon extension allows for nicer argument formatting.
+    "sphinx.ext.napoleon",
+    # add sphinx gallery
+    # "sphinx_gallery.gen_gallery",
+    # citations
+    "myst_parser",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+html_theme_options = {
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/loop3d/LoopStructural",
+            "icon": "fab fa-github-square",
+        },
+        {
+            "name": "Twitter",
+            "url": "https://twitter.com/loop3d",
+            "icon": "fab fa-twitter-square",
+        },
+    ],
+    #     "navbar_start": ["navbar-logo", "navbar-version"],
+    #     "use_edit_page_button": True,
+    "external_links": [
+        {"name": "Loop3d", "url": "https://www.loop3d.org"},
+    ],
+}
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "pydata_sphinx_theme"
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
---
+++ docs/source/index.rst
@@ -0,0 +1,32 @@
+.. Resources documentation master file, created by
+   sphinx-quickstart on Thu Oct 26 10:15:20 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Resources's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+  .. toctree::
+   :caption: LoopResources API
+   :hidden:
+
+.. autosummary::
+   :caption: API
+   :toctree: _autosummary
+   :template: custom-module-template.rst
+   :recursive:
+
+   loopresources
