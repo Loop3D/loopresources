9863eb448c87bd786a7b39ec367b92fcb5c7eb7c
Lachlan Grose <lachlan.grose@monash.edu>
2025-01-07 09:53:32 +1100

    adding tools for converting from alpha beta to strike plane normal and lineation

--- loopresources/drillhole/__init__.py
+++ loopresources/drillhole/__init__.py
@@ -1,6 +1,7 @@
 from .dhconfig import DhConfig
-from .drillhole import desurvey
+from .drillhole import desurvey, DrillHole
 from .dataframeinterpolator import DataFrameInterpolator
 
 from .resample import resample_interval, resample_point, desurvey_point
 from .drillholedb import DrillHoleDB
+from .orientation import alphaBeta2vector, alphaBetaGamma2vector
